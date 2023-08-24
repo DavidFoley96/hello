cd756670156edcb3f7678558ad45a2b6d54059e9
DavidFoley96 <dfoley5@huskers.unl.edu>
2023-08-24 17:05:26 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 /*
 *Name: David Foley
 *Date: 2023-08-24
-*A simple program that prints my name to the console
+*Description: A simple program that prints my name to the console
 */
 
 #include <stdio.h>
 
 int main(void){
-    printf("David Foley\n");
+    printf("David Foley!\n");
+    printf("Computer Engineering\n");
 }
